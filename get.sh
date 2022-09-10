005080720157a2e8ddba6452cdb487db5b9dbd16
Stefan Cristan Hantig <cristianhantig96@gmail.com>
2022-09-10 22:51:32 +0300

    add automatic input download

---
+++ get.sh
@@ -0,0 +1,11 @@
+. .cookie.sh
+
+YEAR=${YEAR:-2021}
+DIR=${DIR:-resources}
+URL=https://adventofcode.com/"$YEAR"/day/"$1"/input
+FILENAME="$DIR/Day$1.txt"
+
+echo "URL=$URL"
+echo "FILENAME=$(realpath $FILENAME)"
+
+curl -S -o "$FILENAME" -b "$AOC_COOKIE"  "$URL"
