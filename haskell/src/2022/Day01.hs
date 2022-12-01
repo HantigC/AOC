e6b440f0efb008156eb81fdd2da8998acc3b1c54
HantigC <cristianhantig96@gmail.com>
2022-12-01 20:51:27 +0200

    add Day01 for 2022

---
+++ haskell/src/2022/Day01.hs
@@ -0,0 +1,24 @@
+module Day01 where
+import           Data.List                      ( sort
+                                                , sortOn
+                                                )
+import           Data.List.Split                ( splitOn )
+import qualified Utils                         as U
+
+toCalories :: [String] -> [[Int]]
+toCalories lines = map (map read) $ splitOn [""] lines
+
+toCaloriesPerElf :: [String] -> [Int]
+toCaloriesPerElf lines = map (sum . map read) $ splitOn [""] lines
+
+partOne :: [String] -> Int
+partOne = maximum . toCaloriesPerElf
+
+partTwo :: [String] -> Int
+partTwo = sum . take 3 . sortOn (\x -> -x) . toCaloriesPerElf
+
+main :: IO ()
+main = do
+    lines <- U.readLines $ U.get2022Resources 1
+    print $ partOne lines
+    print $ partTwo lines
