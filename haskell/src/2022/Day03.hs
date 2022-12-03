a6802fdc04330d08938d57478b296bc84e909ed2
HantigC <cristianhantig96@gmail.com>
2022-12-03 15:15:29 +0200

    add Day3 for 2022

---
+++ haskell/src/2022/Day03.hs
@@ -0,0 +1,38 @@
+module Day03 where
+import           Data.Char                      ( isUpper
+                                                , ord
+                                                )
+import           Data.List                      ( intersect )
+import           Data.List.Split                ( chunksOf )
+import           Utils                          ( get2022Resources
+                                                , readLines
+                                                )
+
+splitHalf xs = splitAt (length xs `div` 2) xs
+
+toPriority c | isUpper c = ord c - ord 'A' + 27
+             | otherwise = ord c - ord 'a' + 1
+
+partOne = sum . map (toPriority . head . uncurry intersect . splitHalf)
+
+partTwo = sum . map (toPriority . head . intersect3) . chunksOf 3
+    where intersect3 [xs, ys, zs] = intersect xs (intersect ys zs)
+
+example =
+    [ "vJrwpWtwJgWrhcsFMMfFFhFp"
+    , "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+    , "PmmdzqPrVvPwwTWBwg"
+    , "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+    , "ttgJtRGJQctTZtZT"
+    , "CrZsJsPPZsGzwwsLwLmpwMDw"
+    ]
+
+main = do
+    lines <- readLines $ get2022Resources 3
+    print $ partOne example
+    print $ partOne lines
+
+
+    print $ partTwo example
+    print $ partTwo lines
+    print "se poate"
